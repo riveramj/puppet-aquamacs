e25b288ffe81a80193670f41bec12f1ba5e76c9e
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 12:19:49 +0200

    Update to latest version

--- spec/classes/aquamacs_spec.rb
+++ spec/classes/aquamacs_spec.rb
@@ -4,6 +4,6 @@ describe 'aquamacs' do
 
   it { should contain_class('aquamacs') }
   it { should contain_package('Aquamacs').with_provider('appdmg') }
-  it { should contain_package('Aquamacs').with_source('http://freefr.dl.sourceforge.net/project/aquamacs/Releases/Aquamacs-Emacs-2.4-UB.dmg') }
+  it { should contain_package('Aquamacs').with_source('http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg') }
 
 end
